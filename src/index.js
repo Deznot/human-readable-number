b789241ce1a4056f178a8d8a312c5cb41fa861a1
Denis <dean.angell@mail.ru>
2021-02-12 17:06:26 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
-}
+    let numbers = ['zero','one','two','three','four','five','six','seven','eight','nine'],
+        numeral = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'],
+        dozen = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'],
+        hundred = ['hundred'];
+    let first = (number % 10);
+    let second = Math.trunc(number / 10) % 10;
+    let third = Math.trunc(number/100);
+    
+    return `${(third !== 0)? numbers[third] +` `+ hundred:''} ${
+            (second !== 0 && second !== 1)?
+                (first !== 0)?
+                    (dozen[second-2] +` `+numbers[first]):
+                    dozen[second-2]:
+            (second == 1)?
+                numeral[first]:
+            (second == 0 && first == 0 && third == 0)?
+                numbers[first]:(first !== 0)?numbers[first]:''}
+    `.trim();
+};
